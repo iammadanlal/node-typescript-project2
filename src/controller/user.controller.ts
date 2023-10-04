e32569a3ab36f14d699cd16d5f5b88a7c0e4ba75
Madan Lal <iammadanlalmali@gmail.com>
2023-10-04 17:19:40 +0530

    verify user feature added

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { CreateUserInput } from "../schema/user.schema";
-import { createUser } from "../service/user.service";
+import { CreateUserInput, verifyUserInput } from "../schema/user.schema";
+import { createUser, findUserById } from "../service/user.service";
 import sendEmail from "../utils/mailer";
 
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput>, res: Response){
@@ -21,3 +21,26 @@ export async function createUserHandler(req: Request<{}, {}, CreateUserInput>, r
     return res.status(500).send(e);
   }
 }
+
+export async function verifyUserHandler(req: Request<verifyUserInput>, res: Response) {
+  const {id, verificationCode} = req.params;
+  // find user
+  const user = await findUserById(id);
+
+  if(!user){
+    return res.send("Could not verify user");
+  }
+
+  if(user.verified){
+    return res.send("User is already verified")
+  }
+
+  if(user.verificationCode = verificationCode){
+    user.verified = true;
+    await user.save();
+    return res.send("User successfully verified")
+  }
+
+  return res.send("Could not verify user");
+}
+
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import validateResource from '../middleware/validateResource';
-import { createUserSchema } from '../schema/user.schema';
-import { createUserHandler } from '../controller/user.controller';
+import { createUserSchema, verifyUserSchema } from '../schema/user.schema';
+import { createUserHandler, verifyUserHandler } from '../controller/user.controller';
 
 const router = express.Router();
 
-router.post('/api/user', validateResource(createUserSchema), createUserHandler);
+router.post('/api/users', validateResource(createUserSchema), createUserHandler);
+router.get('/api/users/verify/:id/:verificationCode', validateResource(verifyUserSchema), verifyUserHandler)
 
 export default router;
